0e3f8972653b709ef8935973e7ed094ab5d43976
SemPlaatsman <semplaatsman@gmail.com>
2024-07-21 23:54:34 +0200

    Finished concept SEO.tsx component

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Helmet } from 'react-helmet';
+import i18n from '../../i18n';
 
 const SEO: React.FC = () => {
   // Possibly add a different canonical URL too
@@ -9,7 +10,9 @@ const SEO: React.FC = () => {
 
   return (
     <Helmet>
+      <html lang={i18n.language} />
       <title>{t('title')}</title>
+      <meta name="author" content={t('author')} />
       <meta name="description" content={t('description')} />
       <meta
         name="keywords"
@@ -22,6 +25,32 @@ const SEO: React.FC = () => {
       <meta property="og:url" content={siteUrl} />
       <meta property="og:title" content={t('title')} />
       <meta property="og:description" content={t('description')} />
+
+      {/* Structured data for a person (adjust as needed) */}
+      <script type="application/ld+json">
+        {JSON.stringify({
+          '@context': 'https://schema.org',
+          '@type': 'Person',
+          name: t('author'),
+          givenName: t('givenName'),
+          familyName: t('familyName'),
+          additionalName: t('additionalName'),
+          description: t('personDescription'),
+          url: siteUrl,
+          jobTitle: t('jobTitle'),
+          sameAs: [t('linkedinUrl'), t('githubUrl'), t('portfolioUrl')],
+          knowsAbout: t('knowsAbout', { returnObjects: true }) as string[],
+          email: t('email'),
+          gender: t('gender'),
+          knowsLanguage: t('knowsLanguage', { returnObjects: true }) as string[],
+          nationality: t('nationality'),
+          address: {
+            '@type': 'PostalAddress',
+            addressLocality: t('addressLocality'),
+            addressCountry: t('addressCountry'),
+          },
+        })}
+      </script>
     </Helmet>
   );
 };
