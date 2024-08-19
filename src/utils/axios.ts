ec286824b32df2d64e9713b6213a3695b1d6d2e3
SemPlaatsman <semplaatsman@gmail.com>
2024-08-19 10:07:26 +0200

    Remove mailjet code

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,11 +1,3 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const mailjetInstance = axios.create({
-  baseURL: 'https://api.mailjet.com/v3.1',
-  auth: {
-    username: import.meta.env.VITE_MAILJET_API_KEY,
-    password: import.meta.env.VITE_MAILJET_SECRET_KEY,
-  },
-});
-
-export { mailjetInstance };
+// Will later be used for the axios GitHub API instance
