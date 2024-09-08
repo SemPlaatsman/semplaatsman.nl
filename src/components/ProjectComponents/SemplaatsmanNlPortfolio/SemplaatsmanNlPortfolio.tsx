648d83718352919500f93fa4418b7fce60f8a378
SemPlaatsman <semplaatsman@gmail.com>
2024-09-08 17:29:35 +0200

    semplaatsman.nl project page placeholder

--- src/components/ProjectComponents/SemplaatsmanNlPortfolio/SemplaatsmanNlPortfolio.tsx
+++ src/components/ProjectComponents/SemplaatsmanNlPortfolio/SemplaatsmanNlPortfolio.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-// import styles from './SemplaatsmanNlPortfolio.module.scss';
+import projectConfig from './projectConfig';
+import styles from './SemplaatsmanNlPortfolio.module.scss';
 
 const SemplaatsmanNlPortfolio: React.FC = () => {
-  return <></>;
+  return (
+    <div>
+      <p>This is just a temporary project page, full documentation will be written soon!</p>
+      <p>
+        In the mean time, you can check out{' '}
+        <a
+          href={projectConfig.codeUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+          aria-label=""
+        >
+          the source code on GitHub
+        </a>
+        !
+      </p>
+    </div>
+  );
 };
 
 export default SemplaatsmanNlPortfolio;
