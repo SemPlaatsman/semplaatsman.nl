eba33229b26b27181c7644016bfb3b057074f874
SemPlaatsman <semplaatsman@gmail.com>
2024-09-28 18:30:43 +0200

    Finished The Felis Foundation project

--- src/components/ProjectComponents/TheFelisFoundation/TheFelisFoundation.tsx
+++ src/components/ProjectComponents/TheFelisFoundation/TheFelisFoundation.tsx
@@ -1,12 +1,60 @@
 import React from 'react';
+import { useTranslation, Trans } from 'react-i18next';
 
-// import projectConfig from './projectConfig';
-// import styles from './TheFelisFoundation.module.scss';
+import projectConfig from './projectConfig';
+import styles from './TheFelisFoundation.module.scss';
 
 const TheFelisFoundation: React.FC = () => {
+  const { t } = useTranslation(`projects/${projectConfig.localeKey}`);
+
+  const overviewDescription: string[] = t('overview.description', { returnObjects: true });
+  const technicalFeatures: string[] = t('technicalImplementation.features', {
+    returnObjects: true,
+  });
+  const keyFeatures: string[] = t('keyFeatures.list', { returnObjects: true });
+
   return (
-    <div>
-      <p>This is just a temporary project page, full documentation will be written soon!</p>
+    <div className={styles.projectContainer}>
+      <h4 className={`h4`}>{t('overview.title')}</h4>
+      {overviewDescription.map((paragraph: string, index: number) => (
+        <p key={index}>
+          <Trans>{paragraph}</Trans>
+        </p>
+      ))}
+
+      <h4 className={`h4`}>{t('technicalImplementation.title')}</h4>
+      <p>
+        <Trans>{t('technicalImplementation.description')}</Trans>
+      </p>
+      <ul>
+        {technicalFeatures.map((feature: string, index: number) => (
+          <li key={index}>
+            <Trans>{feature}</Trans>
+          </li>
+        ))}
+      </ul>
+
+      <h4 className={`h4`}>{t('keyFeatures.title')}</h4>
+      <p>
+        <Trans>{t('keyFeatures.description')}</Trans>
+      </p>
+      <ul>
+        {keyFeatures.map((feature: string, index: number) => (
+          <li key={index}>
+            <Trans>{feature}</Trans>
+          </li>
+        ))}
+      </ul>
+
+      <h4 className={`h4`}>{t('architecture.title')}</h4>
+      <p>
+        <Trans>{t('architecture.description')}</Trans>
+      </p>
+
+      <h4 className={`h4`}>{t('results.title')}</h4>
+      <p>
+        <Trans>{t('results.description')}</Trans>
+      </p>
     </div>
   );
 };
